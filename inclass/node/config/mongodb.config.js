7fbb11aff70e7d10b8fb273ccc73d8e7175da77f
1909802728861 <cshuhand@gmail.com>
2023-01-11 11:39:25 +0700

    Change url server

--- inclass/node/config/mongodb.config.js
+++ inclass/node/config/mongodb.config.js
@@ -1,3 +1,7 @@
+// module.exports = {
+//     url: 'mongodb://127.0.0.1:27017/customercrud'
+// }
+
 module.exports = {
-    url: 'mongodb://127.0.0.1:27017/customercrud'
+    url: 'mongodb+srv://Admin:1909802728861@clsoutheastasia.rtrfd7l.mongodb.net/?retryWrites=true&w=majority'
 }
